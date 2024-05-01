95c6319b56102562731edc4412f0f2bf80360421
zhaohz7 <98800756+zhaohz7@users.noreply.github.com>
2024-05-01 19:58:04 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,20 @@
 makeCacheMatrix <- function(x = matrix()) {
     #initialize a novel 'cached matrix' object and has four inner functions: set, get,
     #set_inverse, and get_inverse. Return a list object.
+    
     #Usage:
-    #
+    #suppose you have a random matrix:
+    #num_rows <- 1000
+    #num_cols <- 1000
+    #random_matrix <- matrix(rnorm(num_rows * num_cols), nrow = num_rows, ncol = num_cols)
+    
+    #initialize a novel 'cached matrix' object and set the random matrix previously generated
+    #cached_matrix <- makeCacheMatrix()
+    #cached_matrix$set(random_matrix)
+
+    #get back the value and check how the matrix stores if needed:
+    #cached_matrix$get(random_matrix)
+
     x_inverse <- NULL
     set <- function(y) {
         x <<- y
@@ -17,6 +29,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 cacheSolve <- function(x, ...) {
+    #calculate the inverse using the cacheSolve() function
+    #when there's no cached inverse, do the calculate and use the set_inverse to store the inverse matrix
+    #otherwise just retrieve using the get_inverse function and message "getting cached inverse matrix"
+    
+    #e.g.
+    #cacheSolve(cached_matrix)
+    
     x_inverse <- x$get_inverse()
     if(!is.null(x_inverse)) {
         message("getting cached inverse matrix")
